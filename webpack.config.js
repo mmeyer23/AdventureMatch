a10f193c6494a789d775530192e85b2d8a6262fc
Mason Meyer <mason.meyer@gmail.com>
2024-09-20 18:20:46 -0700

    webpack is set up

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body',
+});
+
+module.exports = {
+  mode: 'development',
+  entry: './client/index.js', // Your entry point
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Output directory
+    filename: 'bundle.js', // Output filename
+  },
+  module: {
+    rules: [
+      { test: /\.jsx?$/, use: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
+  plugins: [HtmlWebpackPluginConfig],
+};
+
+//webpack.config.js
+
+// const path = require('path');
+
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+//   template: './client/index.html',
+//   filename: 'index.html',
+//   inject: 'body'
+// })
+
+// module.exports = {
+//     mode: 'development',
+//   entry: './client/index.js',
+//   output: {
+//     path: path.resolve('dist'),
+//     filename: 'index_bundle.js'
+//   },
+//   module: {
+//     rules: [
+//       { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+//       { test: /\.jsx$/, use: 'babel-loader', exclude: /node_modules/ },
+//       { test: /\.css$/, use: ['style-loader','css-loader'],
+//       },
+//     ]
+//   },
+//   plugins: [HtmlWebpackPluginConfig]
+// }
