cda2d7ea181bd163aa1b6e588605e3f0500c26ce
Mason Meyer <mason.meyer@gmail.com>
2024-09-21 13:43:21 -0700

    database connection done

--- client/component/login.jsx
+++ client/component/login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router.dom';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
   return <h2>This is login</h2>;
--- client/component/main.jsx
+++ client/component/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router.dom';
+import { Link } from 'react-router-dom';
 
 export default function Main() {
   return <h2>This is main</h2>;
